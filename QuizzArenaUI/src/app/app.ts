5616bc0c12ceafdd502deaab0280069dec54c2db
bsalinasjala <bruno.salinas@jala.university>
2026-05-21 11:00:17 -0400

    Update app.spec.ts component files

--- QuizzArenaUI/src/app/app.ts
+++ QuizzArenaUI/src/app/app.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Button } from './shared/atoms/button/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Button],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css',
+  styleUrl: './app.css'
 })
-export class App {}
+export class App {
+  protected readonly title = signal('QuizzArenaUI');
+}
