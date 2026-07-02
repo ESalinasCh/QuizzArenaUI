ef3a9f2a737624730061724afecb623ade3a6982
German-Jala <German.Alvarez@jala.university>
2026-07-02 09:45:18 -0400

    feat: initial question page was made

---
+++ QuizzArenaUI/src/app/features/teacher/components/exam-filter-modal/exam-filter-modal.ts
@@ -0,0 +1,44 @@
+import { ChangeDetectionStrategy, Component, input, linkedSignal, output } from '@angular/core';
+import { Button } from '../../../../shared/atoms/button/button';
+import { ExamStatus } from '../../models/exam.model';
+import { TextInput } from "../../../../shared/molecules/text-input/text-input";
+import { ModalTemplateComponent } from '../../../../shared/organisms/modal-template/modal-template';
+import { form, FormField } from '@angular/forms/signals';
+import { CheckboxInputComponent } from "../../../../shared/molecules/checkbox-input/checkbox-input";
+import { ExamFormFilter } from '../../models/exam-form-filter';
+
+@Component({
+    selector: 'qz-exam-filter-modal',
+    imports: [Button, TextInput, ModalTemplateComponent, FormField, CheckboxInputComponent],
+    templateUrl: './exam-filter-modal.html',
+})
+export class ExamFilterModal {
+    protected handleToogleModal = output<void>();
+    protected handleNewFilter = output<ExamFormFilter>();
+    validFilter = input<ExamFormFilter>(new ExamFormFilter());
+    protected filterModel = linkedSignal(() => ({ ...this.validFilter() }));
+    protected filterForm = form<ExamFormFilter>(this.filterModel);
+    protected stateOptions: { key: ExamStatus, label: string }[] = [
+        { key: 'draft', label: 'Draft' },
+        { key: 'published', label: 'Published' },
+    ];
+
+    protected applyFilters(): void {
+        this.handleNewFilter.emit({
+            endDate: this.filterModel().endDate,
+            startDate: this.filterModel().startDate,
+            states: this.filterModel().states,
+        });
+        this.toogleFilterModal();
+    }
+
+    protected cleanFilters(): void {
+        this.handleNewFilter.emit(new ExamFormFilter());
+        this.toogleFilterModal();
+    }
+
+    protected toogleFilterModal() {
+        this.handleToogleModal.emit();
+    }
+
+}
