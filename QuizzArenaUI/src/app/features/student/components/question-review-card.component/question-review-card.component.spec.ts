e4af85a3466e0262b12a3301422a7b463c737580
RoBra18 <brand8n123@gmail.com>
2026-08-06 02:14:22 -0400

    fix: imlement tests

--- QuizzArenaUI/src/app/features/student/components/question-review-card.component/question-review-card.component.spec.ts
+++ QuizzArenaUI/src/app/features/student/components/question-review-card.component/question-review-card.component.spec.ts
@@ -5,7 +5,6 @@ import { QuestionReviewCardComponent } from './question-review-card.component';
 describe('QuestionReviewCardComponent', () => {
   let component: QuestionReviewCardComponent;
   let fixture: ComponentFixture<QuestionReviewCardComponent>;
-
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [QuestionReviewCardComponent],
@@ -13,9 +12,21 @@ describe('QuestionReviewCardComponent', () => {
 
     fixture = TestBed.createComponent(QuestionReviewCardComponent);
     component = fixture.componentInstance;
-    await fixture.whenStable();
-  });
 
+    fixture.componentRef.setInput('question', {
+      id: 'q1',
+      content: 'Question',
+      selectedOptionIds: [],
+      isCorrect: null,
+      justification: null,
+      options: [],
+    });
+
+    fixture.componentRef.setInput('questionNumber', 1);
+    fixture.componentRef.setInput('totalQuestions', 10);
+
+    fixture.detectChanges();
+  });
   it('should create', () => {
     expect(component).toBeTruthy();
   });
